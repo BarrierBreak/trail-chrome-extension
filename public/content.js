fcd46ed1686b45b3deed9563bcc8dd0f950c5766
Faraaz <faraaz@barrierbreak.com>
2024-04-09 16:20:57 +0530

    Add side panel with api handling

--- public/content.js
+++ public/content.js
@@ -1,55 +1,38 @@
 /*global chrome*/
 
-// chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
-//   console.log(response.farewell);
-// });
-
-// console.log("side-panel script loaded");
-
-// chrome.runtime.onMessage.addListener(function (msg) {
-//   if (msg == "toggle") {
-//     console.log("message received");
-//     toggle();
-//   }
-// });
-
-// var iframe = document.createElement("iframe");
-// // iframe.style.background = "green";
-// iframe.style.height = "100%";
-// iframe.style.width = "400px";
-// iframe.style.position = "fixed";
-// iframe.style.top = "0px";
-// iframe.style.right = "0px";
-// iframe.style.zIndex = "9000000000000000000";
-// iframe.style.border = "0px";
-// iframe.src = chrome.runtime.getURL("index.html");
-
-// document.body.appendChild(iframe);
-
-// function toggle() {
-//   if (iframe.style.width == "0px") {
-//     iframe.style.width = "400px";
-//   } else {
-//     iframe.style.width = "0px";
-//   }
-// }
-
-const body = document.body.outerHTML;
-console.log(body);
-
+//To inject Trail extension button on load
 const extensionBtn = new DOMParser().parseFromString(
-  "<button id='trail-btn' style = 'position: fixed; width: 92px; height: 40px; font-size: 20px; background: #0000FF; top: 244px; right: -26px; border-radius: 4px 4px 0px 0px; border: 0px; cursor: pointer; color: #fff; transform: rotate(-90deg); z-index: 9999}'>Trail</button>",
+  "<button id='trail-btn' style = 'display: flex; align-items: center; justify-content: center; gap: 5px; position: fixed; width: 92px; height: 40px; font-size: 16px; color: #FEFEFE; background: #5928ED; border: 1px solid #FEFEFE; border-bottom: 0; top: 244px; right: -26px; border-radius: 4px 4px 0px 0px; cursor: pointer; transform: rotate(-90deg); z-index: 9999'><svg xmlns='http://www.w3.org/2000/svg' width='39' height='16' viewBox='0 0 39 16' fill='none'><path d='M0.101074 3.62114V1.40676H10.387V3.62114H6.4481V15.3021H3.95768V3.62114H0.101074Z' fill='#FEFEFE'/><path d='M10.4897 4.79113H12.4895L12.7348 6.29492C13.1835 5.12492 14.1834 4.47626 15.2034 4.47626C15.8151 4.47626 16.4486 4.62251 16.8368 4.87372L16.4083 6.94186C15.9999 6.73367 15.4908 6.56505 14.9597 6.56505C13.8574 6.56505 13.1432 7.46319 13.1028 8.67448V15.2987H10.6746V6.4601L10.4914 4.78769L10.4897 4.79113Z' fill='#FEFEFE'/><path d='M17.2451 10.4966C17.2451 6.81796 19.0213 4.47798 22.5301 4.47798C23.2241 4.47798 24.7129 4.64488 26.1632 5.20923V13.2959L26.3666 15.3021H24.2643L24.1214 14.1941L24.0391 14.1734C23.488 14.989 22.5906 15.6153 21.2227 15.6153C18.1408 15.6153 17.2434 13.234 17.2434 10.4966H17.2451ZM23.7753 11.9797V6.56849C23.4694 6.42224 22.8561 6.27599 22.2242 6.27599C20.285 6.27599 19.633 8.15658 19.633 10.414C19.633 12.1277 20.0615 13.7777 21.7554 13.7777C23.0611 13.7777 23.7753 12.8796 23.7753 11.9814V11.9797Z' fill='#FEFEFE'/><path d='M28.5088 1.76119C28.5088 1.0093 29.0599 0.381287 29.8968 0.381287C30.7337 0.381287 31.305 0.986933 31.305 1.76119C31.305 2.53545 30.7135 3.16174 29.8968 3.16174C29.0801 3.16174 28.5088 2.57675 28.5088 1.76119ZM31.1218 4.79113V15.3021H28.6936V4.79113H31.1218Z' fill='#FEFEFE'/><path d='M33.6531 11.8952V0.778549H36.1014V11.8539C36.1014 13.1701 36.367 13.6502 37.2038 13.6502C37.592 13.6502 38.0406 13.5039 38.4692 13.3164L39.0002 14.9888C38.4289 15.3863 37.4492 15.6151 36.572 15.6151C33.8178 15.6151 33.6531 13.7758 33.6531 11.8952Z' fill='#FEFEFE'/></svg> |<svg style = 'transform: rotate(90deg);' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M11.1499 18.415V20.2856H12.8202V18.415C12.8202 16.1428 13.5252 15.0743 19.9575 15.0743H23.6026C22.244 20.2113 17.5657 24 12.0005 24C6.43537 24 1.71775 20.1793 0.380914 15.0114H10.8443V13.341H0.0753561C0.0247732 12.9002 0 12.4522 0 12C0 11.5478 0.0247732 11.0998 0.0753561 10.659H3.99397C7.37992 10.659 9.29589 10.3864 10.609 9.72058C12.1037 8.96181 12.8006 7.66724 12.8006 5.64797V3.77529H11.1303V5.64797C11.1303 7.9181 10.4252 8.98864 3.99397 8.98864H0.380914C1.71671 3.81865 6.41266 0 12.0005 0C17.5884 0 22.3215 3.84859 23.6346 9.05059H13.1061V10.7209H23.9319C23.9773 11.1421 24 11.5675 24 12C24 12.4759 23.9711 12.9436 23.9184 13.405H19.9575C14.1157 13.405 11.1499 14.1803 11.1499 18.416V18.415Z' fill='#FEFEFE'/></svg></button>",
   "text/html"
 ).body.firstElementChild;
-document.body.appendChild(extensionBtn);
+document.body.insertBefore(extensionBtn, document.body.firstChild);
 
+//To inject Trail extension iframe on load
+const iframe = new DOMParser().parseFromString(
+  "<iframe id='trail-iframe' title='trail-extension' style = 'width: 0px; height: 100vh; position: fixed; top: 0; right: 0; z-index: 9999'></iframe>",
+  "text/html"
+).body.firstElementChild;
+iframe.src = chrome.runtime.getURL("index.html");
+document.body.insertBefore(iframe, document.body.firstChild.nextSibling);
+
+//Onclick functionality for extension button
 extensionBtn.addEventListener("click", () => {
-  console.log("click");
-  extensionBtn.style.visibility = "hidden";
-  const iframe = new DOMParser().parseFromString(
-    "<iframe id='trail-iframe' style = 'width: 324px; height: 100vh; background: beige; position: fixed; top: 0; right: 0; z-index: 9999'></iframe>",
-    "text/html"
-  ).body.firstElementChild;
-  iframe.src = chrome.runtime.getURL("index.html");
-  document.body.appendChild(iframe);
+  showIframe();
+
+  window.addEventListener("message", (event) => {
+    if (event.data === "close-button-clicked") {
+      hideIframe();
+    }
+  });
 });
+
+//Functions to show extension iframe
+function showIframe() {
+  iframe.style.width = "324px";
+}
+
+//Functions to hide extension iframe
+function hideIframe() {
+  iframe.style.width = "0px";
+  document.querySelector("#trail-btn").focus();
+}
--- src/Extension.tsx
+++ src/Extension.tsx
@@ -1,11 +1,112 @@
 import { Button } from "@trail-ui/react";
+import { useState } from "react";
 
 function Extension() {
+  const [responseData, setResponseData] = useState([]);
+  const postData = async () => {
+    await fetch(
+      "http://ec2-65-0-110-95.ap-south-1.compute.amazonaws.com:3000/audit",
+      {
+        method: "POST",
+        headers: {
+          Accept: "*/*",
+          "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+          "x-api-key":
+            "91b75b7e4f4a0036ee3c942838065bae8891820bf2b15e7246cffa04da0922f3",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          url: "https://barrierbreak.com",
+          element: "",
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        setResponseData(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const handleClick = () => {
+    window.parent.postMessage("close-button-clicked", "*");
+  };
+
+  const handleTestResults = () => {
+    postData();
+    console.log("data", responseData);
+  };
+
   return (
     <>
-      <div className="w-40">
-        <h1>From react page</h1>
-        <Button appearance="primary">Add extension</Button>
+      <div className="w-[324px]">
+        <div className="flex justify-between items-center border-b border-neutral-300 pt-[6px] pr-[20px] pb-[6px] pl-[24px]">
+          <div className="flex items-center">
+            <svg
+              className="mr-[6px]"
+              xmlns="http://www.w3.org/2000/svg"
+              width="40"
+              height="40"
+              viewBox="0 0 40 40"
+              fill="none"
+            >
+              <path
+                d="M18.5832 30.6917V33.8094H21.3669V30.6917C21.3669 26.9047 22.542 25.1239 33.2625 25.1239H39.3376C37.0734 33.6855 29.2761 40 20.0009 40C10.7256 40 2.86291 33.6321 0.634856 25.0189H18.0739V22.235H0.125593C0.0412887 21.5003 0 20.7536 0 20C0 19.2464 0.0412887 18.4997 0.125593 17.765H6.65662C12.2999 17.765 15.4931 17.3107 17.6816 16.201C20.1729 14.9363 21.3343 12.7787 21.3343 9.41329V6.29215H18.5504V9.41329C18.5504 13.1968 17.3754 14.9811 6.65662 14.9811H0.634856C2.86119 6.36442 10.6878 0 20.0009 0C29.3139 0 37.2025 6.41432 39.3909 15.0843H21.8435V17.8682H39.8864C39.9621 18.5702 40 19.2791 40 20C40 20.7932 39.9518 21.5726 39.8641 22.3417H33.2625C23.5261 22.3417 18.5832 23.6339 18.5832 30.6934V30.6917Z"
+                fill="#5928ED"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="39"
+              height="16"
+              viewBox="0 0 39 16"
+              fill="none"
+            >
+              <path
+                d="M0.101074 3.62114V1.40676H10.387V3.62114H6.4481V15.3021H3.95768V3.62114H0.101074Z"
+                fill="#302E38"
+              />
+              <path
+                d="M10.4897 4.79113H12.4895L12.7348 6.29492C13.1835 5.12492 14.1834 4.47626 15.2034 4.47626C15.8151 4.47626 16.4486 4.62251 16.8368 4.87372L16.4083 6.94186C15.9999 6.73367 15.4908 6.56505 14.9597 6.56505C13.8574 6.56505 13.1432 7.46319 13.1028 8.67448V15.2987H10.6746V6.4601L10.4914 4.78769L10.4897 4.79113Z"
+                fill="#302E38"
+              />
+              <path
+                d="M17.2451 10.4966C17.2451 6.81796 19.0213 4.47798 22.5301 4.47798C23.2241 4.47798 24.7129 4.64488 26.1632 5.20923V13.2959L26.3666 15.3021H24.2643L24.1214 14.1941L24.0391 14.1734C23.488 14.989 22.5906 15.6153 21.2227 15.6153C18.1408 15.6153 17.2434 13.234 17.2434 10.4966H17.2451ZM23.7753 11.9797V6.56849C23.4694 6.42224 22.8561 6.27599 22.2242 6.27599C20.285 6.27599 19.633 8.15658 19.633 10.414C19.633 12.1277 20.0615 13.7777 21.7554 13.7777C23.0611 13.7777 23.7753 12.8796 23.7753 11.9814V11.9797Z"
+                fill="#302E38"
+              />
+              <path
+                d="M28.5088 1.76119C28.5088 1.0093 29.0599 0.381287 29.8968 0.381287C30.7337 0.381287 31.305 0.986933 31.305 1.76119C31.305 2.53545 30.7135 3.16174 29.8968 3.16174C29.0801 3.16174 28.5088 2.57675 28.5088 1.76119ZM31.1218 4.79113V15.3021H28.6936V4.79113H31.1218Z"
+                fill="#302E38"
+              />
+              <path
+                d="M33.6531 11.8952V0.778549H36.1014V11.8539C36.1014 13.1701 36.367 13.6502 37.2038 13.6502C37.592 13.6502 38.0406 13.5039 38.4692 13.3164L39.0002 14.9888C38.4289 15.3863 37.4492 15.6151 36.572 15.6151C33.8178 15.6151 33.6531 13.7758 33.6531 11.8952Z"
+                fill="#302E38"
+              />
+            </svg>
+          </div>
+          <button onClick={handleClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+            >
+              <path
+                d="M16.0001 25.3333C10.8454 25.3333 6.66675 21.1546 6.66675 16C6.66675 10.8453 10.8454 6.66666 16.0001 6.66666C21.1547 6.66666 25.3334 10.8453 25.3334 16C25.3334 21.1546 21.1547 25.3333 16.0001 25.3333ZM16.0001 14.6801L13.3602 12.0402L12.0403 13.3601L14.6802 16L12.0403 18.6398L13.3602 19.9598L16.0001 17.3199L18.6399 19.9598L19.9598 18.6398L17.32 16L19.9598 13.3601L18.6399 12.0402L16.0001 14.6801Z"
+                fill="#484453"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="flex justify-center items-center h-screen">
+          <Button appearance="primary" onPress={handleTestResults}>
+            Test Website
+          </Button>
+        </div>
       </div>
     </>
   );
