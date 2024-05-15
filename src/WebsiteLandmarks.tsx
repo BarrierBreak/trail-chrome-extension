d4ce57e36ad3f4247982dd38bfbe07ce7893b942
Faraaz <faraaz@barrierbreak.com>
2024-05-15 09:54:12 +0530

    refactor: :art: code formatting using prettier

--- src/WebsiteLandmarks.tsx
+++ src/WebsiteLandmarks.tsx
@@ -1,24 +1,25 @@
-const WebsiteLandmarks = ({html}: {html: string}) => {
-  const parser = new DOMParser()
-  const doc = parser.parseFromString(html, 'text/html')
+const WebsiteLandmarks = ({ html }: { html: string }) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
 
+  // To get all landmarks of the webpage
   const getLandMarks = (doc: Document) => {
-    const headings = Array.from(doc.querySelectorAll('h1, h2, h3, h4, h5, h6'))
+    const headings = Array.from(doc.querySelectorAll("h1, h2, h3, h4, h5, h6"));
     const landmarks = headings.map((heading) => {
-      const tagName = heading.tagName
-      const level = parseInt(heading.tagName[1])
-      const text = heading.textContent
+      const tagName = heading.tagName;
+      const level = parseInt(heading.tagName[1]);
+      const text = heading.textContent;
       return {
         tagName,
         level,
         text,
-      }
-    })
+      };
+    });
 
-    return landmarks
-  }
+    return landmarks;
+  };
 
-  const landMarks = getLandMarks(doc)
+  const landMarks = getLandMarks(doc);
 
   return (
     <div>
@@ -29,23 +30,27 @@ const WebsiteLandmarks = ({html}: {html: string}) => {
             <div
               style={{
                 marginLeft: landmark.level * 20,
-                fontWeight: 'bold',
-                color: 'blue',
-                fontSize: '1.3em',
+                fontWeight: "bold",
+                color: "blue",
+                fontSize: "1.3em",
               }}
             >
               {landmark.tagName}
             </div>
-            <div style={{
-              marginLeft: landmark.level * 20,
-              color: 'black',
-              fontSize: '1.2em',
-            }}>{landmark.text}</div>
+            <div
+              style={{
+                marginLeft: landmark.level * 20,
+                color: "black",
+                fontSize: "1.2em",
+              }}
+            >
+              {landmark.text}
+            </div>
           </>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WebsiteLandmarks
+export default WebsiteLandmarks;
