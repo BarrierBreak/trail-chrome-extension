223d433121d226ac54c5ab8d36b0ae483f46f9a4
Criston <criston@barrierbreak.com>
2024-06-18 19:50:58 +0530

    feat(app): :sparkles: add heading list landmarks alttext detectors

--- src/Extension.tsx
+++ src/Extension.tsx
@@ -1,200 +1,204 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from "react";
+import {useEffect, useState} from 'react'
 import {
   Button,
   Checkbox,
   Chip,
   IconButton,
-  Switch,
   Tab,
   TabList,
   TabPanel,
   Tabs,
-} from "@trail-ui/react";
+} from '@trail-ui/react'
 import {
   ExportIcon,
   TrailIcon,
   TrailAMSVerticalIcon,
   MinusIcon,
-} from "@trail-ui/icons";
-import WebsiteLandmarks from "./WebsiteLandmarks";
-import CheckboxTable from "./CheckboxTable";
-import DownloadCSV from "./DownloadCSV";
+} from '@trail-ui/icons'
+import WebsiteLandmarks from './WebsiteLandmarks'
+import CheckboxTable from './CheckboxTable'
+import DownloadCSV from './DownloadCSV'
 
 export interface IssueItems {
   issues: {
     clip: {
-      x: number;
-      y: number;
-      width: number;
-      height: number;
-    };
-    clipBase64: string;
-    code: string;
-    context: string;
-    elementTagName: string;
-    id: string;
-    message: string;
-    recurrence: number;
-    selector: string;
-    type: string;
-    typeCode: number;
-  }[];
+      x: number
+      y: number
+      width: number
+      height: number
+    }
+    clipBase64: string
+    code: string
+    context: string
+    elementTagName: string
+    id: string
+    message: string
+    recurrence: number
+    selector: string
+    type: string
+    typeCode: number
+  }[]
 }
 
 export interface Issues {
   issues: {
     errors: {
-      code: string;
-      conformance_level: string;
-      criteria_name: string;
-      element: string;
-      failing_issue_variable: string;
-      failing_technique: string;
-      id: string;
+      code: string
+      conformance_level: string
+      criteria_name: string
+      element: string
+      failing_issue_variable: string
+      failing_technique: string
+      id: string
       issues: {
         clip: {
-          x: number;
-          y: number;
-          width: number;
-          height: number;
-        };
-        clipBase64: string;
-        code: string;
-        context: string;
-        elementTagName: string;
-        id: string;
-        message: string;
-        recurrence: number;
-        selector: string;
-        type: string;
-        typeCode: number;
-      }[];
-      message: string;
-      occurences: string;
-      rule_name: string;
-      severity: string;
-    }[];
+          x: number
+          y: number
+          width: number
+          height: number
+        }
+        clipBase64: string
+        code: string
+        context: string
+        elementTagName: string
+        id: string
+        message: string
+        recurrence: number
+        selector: string
+        type: string
+        typeCode: number
+      }[]
+      message: string
+      occurences: string
+      rule_name: string
+      severity: string
+    }[]
     warnings: {
-      code: string;
-      conformance_level: string;
-      criteria_name: string;
-      element: string;
-      failing_issue_variable: string;
-      failing_technique: string;
-      id: string;
+      code: string
+      conformance_level: string
+      criteria_name: string
+      element: string
+      failing_issue_variable: string
+      failing_technique: string
+      id: string
       issues: {
         clip: {
-          x: number;
-          y: number;
-          width: number;
-          height: number;
-        };
-        clipBase64: string;
-        code: string;
-        context: string;
-        elementTagName: string;
-        id: string;
-        message: string;
-        recurrence: number;
-        selector: string;
-        type: string;
-        typeCode: number;
-      }[];
-      message: string;
-      occurences: string;
-      rule_name: string;
-      severity: string;
-    }[];
+          x: number
+          y: number
+          width: number
+          height: number
+        }
+        clipBase64: string
+        code: string
+        context: string
+        elementTagName: string
+        id: string
+        message: string
+        recurrence: number
+        selector: string
+        type: string
+        typeCode: number
+      }[]
+      message: string
+      occurences: string
+      rule_name: string
+      severity: string
+    }[]
     pass: {
-      code: string;
-      conformance_level: string;
-      criteria_name: string;
-      element: string;
-      failing_issue_variable: string;
-      failing_technique: string;
-      id: string;
+      code: string
+      conformance_level: string
+      criteria_name: string
+      element: string
+      failing_issue_variable: string
+      failing_technique: string
+      id: string
       issues: {
         clip: {
-          x: number;
-          y: number;
-          width: number;
-          height: number;
-        };
-        clipBase64: string;
-        code: string;
-        context: string;
-        elementTagName: string;
-        id: string;
-        message: string;
-        recurrence: number;
-        selector: string;
-        type: string;
-        typeCode: number;
-      }[];
-      message: string;
-      occurences: string;
-      rule_name: string;
-      severity: string;
-    }[];
+          x: number
+          y: number
+          width: number
+          height: number
+        }
+        clipBase64: string
+        code: string
+        context: string
+        elementTagName: string
+        id: string
+        message: string
+        recurrence: number
+        selector: string
+        type: string
+        typeCode: number
+      }[]
+      message: string
+      occurences: string
+      rule_name: string
+      severity: string
+    }[]
     notices: {
-      code: string;
-      conformance_level: string;
-      criteria_name: string;
-      element: string;
-      failing_issue_variable: string;
-      failing_technique: string;
-      id: string;
+      code: string
+      conformance_level: string
+      criteria_name: string
+      element: string
+      failing_issue_variable: string
+      failing_technique: string
+      id: string
       issues: {
         clip: {
-          x: number;
-          y: number;
-          width: number;
-          height: number;
-        };
-        clipBase64: string;
-        code: string;
-        context: string;
-        elementTagName: string;
-        id: string;
-        message: string;
-        recurrence: number;
-        selector: string;
-        type: string;
-        typeCode: number;
-      }[];
-      message: string;
-      occurences: string;
-      rule_name: string;
-      severity: string;
-    }[];
-  };
+          x: number
+          y: number
+          width: number
+          height: number
+        }
+        clipBase64: string
+        code: string
+        context: string
+        elementTagName: string
+        id: string
+        message: string
+        recurrence: number
+        selector: string
+        type: string
+        typeCode: number
+      }[]
+      message: string
+      occurences: string
+      rule_name: string
+      severity: string
+    }[]
+  }
 }
 
 const Extension = () => {
-  const [responseData, setResponseData] = useState<Issues>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isToggleSelected, setIsToggleSelected] = useState<boolean>(false);
-  const [isCheckboxSelected, setIsCheckboxSelected] = useState<boolean>(false);
+  const [responseData, setResponseData] = useState<Issues>()
+  const [isLoading, setIsLoading] = useState(false)
+  const [isToggleSelected, setIsToggleSelected] = useState<boolean>(false)
+  const [isCheckboxSelected, setIsCheckboxSelected] = useState<boolean>(false)
+  const [isHeadingSelected, setIsHeadingSelected] = useState<boolean>(false)
+  const [isListToggleSelected, setIsListToggleSelected] =
+    useState<boolean>(false)
+  const [isLandmarksSelected, setIsLandmarksSelected] = useState<boolean>(false)
+  const [isAltTextSelected, setIsAltTextSelected] = useState<boolean>(false)
 
-  const [html, setHtml] = useState("");
-  const apiKey = localStorage.getItem("authtoken");
-  const [dataFromChild, setDataFromChild] = useState({});
+  const [html, setHtml] = useState('')
+  const apiKey = localStorage.getItem('authtoken')
+  const [dataFromChild, setDataFromChild] = useState({})
 
   const tabData = [
-    { id: "FAIL", label: "Fail", issues: responseData?.issues.errors.length },
+    {id: 'FAIL', label: 'Fail', issues: responseData?.issues.errors.length},
     {
-      id: "MANUAL",
-      label: "Manual",
+      id: 'MANUAL',
+      label: 'Manual',
       issues: responseData?.issues.warnings.length,
     },
-    { id: "PASS", label: "Pass", issues: responseData?.issues.pass.length },
+    {id: 'PASS', label: 'Pass', issues: responseData?.issues.pass.length},
     {
-      id: "BEST-PRACTICE",
-      label: "BP",
+      id: 'BEST-PRACTICE',
+      label: 'BP',
       issues: responseData?.issues.notices.length,
     },
-  ];
+  ]
 
   // To scrape HTML source code from current tab of webpage
   useEffect(() => {
@@ -202,26 +206,26 @@ const Extension = () => {
       const [tab] = await chrome.tabs.query({
         active: true,
         currentWindow: true,
-      });
+      })
       chrome.scripting.executeScript(
         {
-          target: { tabId: tab.id! },
+          target: {tabId: tab.id!},
           func: () => {
-            const html = document.documentElement.outerHTML;
-            return html;
+            const html = document.documentElement.outerHTML
+            return html
           },
         },
         (results) => {
-          setHtml(results[0].result as string);
+          setHtml(results[0].result as string)
         }
-      );
+      )
     }
 
-    getCurrentTabHtmlSource();
-  }, []);
+    getCurrentTabHtmlSource()
+  }, [])
 
   const postURL = () => {
-    setIsLoading(true);
+    setIsLoading(true)
 
     chrome.tabs.query(
       {
@@ -229,96 +233,124 @@ const Extension = () => {
         currentWindow: true,
       },
       async function (tabs) {
-        const tabURL = tabs[0].url;
-        await fetch("https://trail-api.barrierbreak.com/api/audit", {
-          method: "POST",
+        const tabURL = tabs[0].url
+        await fetch('https://trail-api.barrierbreak.com/api/audit', {
+          method: 'POST',
           headers: {
-            Accept: "*/*",
-            "User-Agent": "BarrierBreak Client (https://www.barrierbreak.com)",
-            "x-api-key": `${apiKey}`,
-            "Content-Type": "application/json",
+            Accept: '*/*',
+            'User-Agent': 'BarrierBreak Client (https://www.barrierbreak.com)',
+            'x-api-key': `${apiKey}`,
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             url: tabURL,
-            element: "all",
+            element: 'all',
           }),
         })
           .then((response) => response.json())
           .then((data) => {
-            console.log("URL:", data);
-            setResponseData(data);
-            setIsLoading(false);
+            console.log('URL:', data)
+            setResponseData(data)
+            setIsLoading(false)
           })
           .catch((error) => {
-            console.error("Error:", error);
-          });
+            console.error('Error:', error)
+          })
       }
-    );
-  };
+    )
+  }
 
   const postData = async () => {
-    setIsLoading(true);
+    setIsLoading(true)
 
     // To get html structure of webpage
-    const htmlDocument = new DOMParser().parseFromString(html, "text/html");
+    const htmlDocument = new DOMParser().parseFromString(html, 'text/html')
 
     // To remove html element having id as 'trail-btn' from DOM
-    const element = htmlDocument.querySelector("#trail-btn");
+    const element = htmlDocument.querySelector('#trail-btn')
     if (element) {
-      element.remove();
+      element.remove()
     }
 
     // To fetch accesibility results of webpage from Scally
-    await fetch("https://trail-api.barrierbreak.com/api/test-html", {
-      method: "POST",
+    await fetch('https://trail-api.barrierbreak.com/api/test-html', {
+      method: 'POST',
       headers: {
-        Accept: "*/*",
-        "User-Agent": "BarrierBreak Client (https://www.barrierbreak.com)",
-        "x-api-key": `${apiKey}`,
-        "Content-Type": "application/json",
+        Accept: '*/*',
+        'User-Agent': 'BarrierBreak Client (https://www.barrierbreak.com)',
+        'x-api-key': `${apiKey}`,
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         html: htmlDocument.documentElement.outerHTML,
-        element: "",
+        element: '',
       }),
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log("Success:", data);
-        setResponseData(data);
-        setIsLoading(false);
+        console.log('Success:', data)
+        setResponseData(data)
+        setIsLoading(false)
       })
       .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
+        console.error('Error:', error)
+      })
+  }
 
   // To handle tab order
   const handleTabOrder = () => {
-    let msg: string;
-    setIsToggleSelected(!isToggleSelected);
-    isToggleSelected ? (msg = "hide-tab-order") : (msg = "show-tab-order");
-    window.parent.postMessage(msg, "*");
-  };
+    let msg: string
+    setIsToggleSelected(!isToggleSelected)
+    isToggleSelected ? (msg = 'hide-tab-order') : (msg = 'show-tab-order')
+    window.parent.postMessage(msg, '*')
+  }
+
+  const handleListToggle = () => {
+    let msg: string
+    setIsListToggleSelected(!isListToggleSelected)
+    isListToggleSelected ? (msg = 'hide-list-tags') : (msg = 'show-list-tags')
+    window.parent.postMessage(msg, '*')
+  }
+
+  const handleLandmarks = () => {
+    let msg: string
+    setIsLandmarksSelected(!isLandmarksSelected)
+    isLandmarksSelected ? (msg = 'hide-landmarks') : (msg = 'show-landmarks')
+    window.parent.postMessage(msg, '*')
+  }
+
+  const handleAltText = () => {
+    let msg: string
+    setIsAltTextSelected(!isAltTextSelected)
+    isAltTextSelected ? (msg = 'hide-alt-text') : (msg = 'show-alt-text')
+    window.parent.postMessage(msg, '*')
+  }
+
+  const handleShowHeading = () => {
+    let msg: string
+    setIsHeadingSelected(!isHeadingSelected)
+    isHeadingSelected ? (msg = 'hide-headings') : (msg = 'show-headings')
+    window.parent.postMessage(msg, '*')
+  }
 
   // To handle minimise functionality
   const handleMinimise = () => {
-    window.parent.postMessage("minimise-button-clicked", "*");
-  };
+    window.parent.postMessage('minimise-button-clicked', '*')
+  }
 
   // API call
   const handleTestResults = () => {
-    isCheckboxSelected ? postData() : postURL();
-  };
+    isCheckboxSelected ? postData() : postURL()
+  }
 
   const handleDataFromChild = (
     data: {
-      id: string;
-      data: IssueItems;
+      id: string
+      data: IssueItems
     }[]
   ) => {
-    setDataFromChild(data);
-  };
+    setDataFromChild(data)
+  }
 
   return (
     <main className="font-poppins">
@@ -340,20 +372,50 @@ const Extension = () => {
             />
           </div>
           <div className="flex gap-4">
-            {responseData?.issues && (
+            {
               <div className="flex items-center gap-2">
-                <p className="text-neutral-600 text-base">Tab Order</p>
-                <Switch
-                  size="sm"
-                  isSelected={isToggleSelected}
-                  onChange={handleTabOrder}
-                  classNames={{ wrapper: "mr-0" }}
-                />
-                <p className="text-neutral-600 text-base">
-                  {isToggleSelected ? "ON" : "OFF"}
-                </p>
+                <div>
+                  <Checkbox
+                    isSelected={isToggleSelected}
+                    onChange={handleTabOrder}
+                  >
+                    Tab Order
+                  </Checkbox>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Checkbox
+                    isSelected={isHeadingSelected}
+                    onChange={handleShowHeading}
+                  >
+                    Headings
+                  </Checkbox>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Checkbox
+                    isSelected={isListToggleSelected}
+                    onChange={handleListToggle}
+                  >
+                    Lists
+                  </Checkbox>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Checkbox
+                    isSelected={isLandmarksSelected}
+                    onChange={handleLandmarks}
+                  >
+                    Landmarks
+                  </Checkbox>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Checkbox
+                    isSelected={isAltTextSelected}
+                    onChange={handleAltText}
+                  >
+                    Alt Text
+                  </Checkbox>
+                </div>
               </div>
-            )}
+            }
             <IconButton
               appearance="default"
               isIconOnly={true}
@@ -371,7 +433,7 @@ const Extension = () => {
               className="before:content-[''] before:h-[1px] before:w-6 before:bg-neutral-300 before:left-0 before:top-[103px] before:fixed
                         after:content-[''] after:h-[1px] after:w-6 after:bg-neutral-300 after:right-0 after:top-[103px] after:fixed"
             >
-              <Tabs color="purple" classNames={{ tab: "border-0 py-0 pr-1.5" }}>
+              <Tabs color="purple" classNames={{tab: 'border-0 py-0 pr-1.5'}}>
                 <div className="flex items-center justify-between h-12 w-[564px] border-b border-neutral-300 sticky z-[1] bg-white top-14">
                   <TabList>
                     {tabData.map((tab) => (
@@ -385,8 +447,8 @@ const Extension = () => {
                             radius="full"
                             children={`${tab.issues}`}
                             classNames={{
-                              base: "p-0 h-[18px] min-w-7 hover:bg-purple-100 active:bg-purple-100",
-                              content: "text-xs text-center px-2 py-0",
+                              base: 'p-0 h-[18px] min-w-7 hover:bg-purple-100 active:bg-purple-100',
+                              content: 'text-xs text-center px-2 py-0',
                             }}
                           />
                         </div>
@@ -462,7 +524,7 @@ const Extension = () => {
         </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Extension;
+export default Extension
